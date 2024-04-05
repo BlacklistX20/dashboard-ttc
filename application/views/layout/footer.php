e6bcb4a38a0184181ea9e3634972d5af9913e4b4
BlacklistX20 <a.syafrul.aswar20@gmail.com>
2024-04-05 19:43:26 +0800

    add DataTables JS Library

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,14 +1,6 @@
     <!-- jQuery -->
     <script src="<?= base_url() ?>asset/js/jquery-3.7.1.min.js"></script>
 
-    <!-- chart.js -->
-    <script src="<?= base_url() ?>asset/src/chart.js-4.4.1/node_modules/chart.js/dist/chart.umd.js"></script>
-    <script src="<?= base_url() ?>asset/src/chart.js-4.4.1/chartjs-plugin-datalabels/chartjs-plugin-datalabels.js"></script>
-
-    <!-- bootstrap .js -->
-    <script src="<?= base_url() ?>asset/src/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
-    <script src="<?= base_url() ?>asset/src/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
-
     <!-- Custom Sidebar Script -->
     <script src="<?= base_url() ?>asset/js/scriptSidebar.js"></script>
 
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -11,9 +11,19 @@
 
   <!-- Bootstrap 5 -->
   <link rel="stylesheet" href="<?= base_url() ?>asset/src/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+  <script src="<?= base_url() ?>asset/src/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+  <script src="<?= base_url() ?>asset/src/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
 
   <!-- Box Icon -->
   <link href='<?= base_url() ?>asset/src/boxicons-2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+  <!-- Data Tables -->
+  <link rel="stylesheet" href="<?= base_url() ?>asset/src/DataTables/datatables.min.css">
+  <script src="<?= base_url() ?>asset/src/DataTables/datatables.min.js"></script>
+
+  <!-- chart.js -->
+  <script src="<?= base_url() ?>asset/src/chart.js-4.4.1/node_modules/chart.js/dist/chart.umd.js"></script>
+  <script src="<?= base_url() ?>asset/src/chart.js-4.4.1/chartjs-plugin-datalabels/chartjs-plugin-datalabels.js"></script>
 
 </head>
 
@@ -23,12 +33,12 @@
       <div>
         <div class="header_toggle nav_logo"> <i class='bx bx-menu' id="header-toggle"></i> </div>
 
-        <div class="nav_list"> 
+        <div class="nav_list">
           <a href="<?= base_url() ?>Pages/index" id="index" class="nav_link"> <i class='bx bx-home nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
           <a href="<?= base_url() ?>Pages/temp" id="temp" class="nav_link"> <i class='bx bxs-thermometer nav_icon'></i> <span class="nav_name">Suhu Ruangan</span> </a>
           <a href="<?= base_url() ?>Pages/electric" id="electric" class="nav_link"> <i class='bx bx-bolt-circle nav_icon'></i> <span class="nav_name">Electricity</span> </a>
           <a href="<?= base_url() ?>Pages/fuel" id="fuel" class="nav_link"> <i class='bx bxs-gas-pump nav_icon'></i> <span class="nav_name">Level BBM</span> </a>
-          <a href="<?= base_url() ?>Pages/potency" id="folder" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Data Potensi</span> </a> 
+          <a href="<?= base_url() ?>Pages/potency" id="folder" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Data Potensi</span> </a>
           <a href="<?= base_url() ?>Pages/space" id="space" class="nav_link"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Space Management</span> </a>
           <a href="<?= base_url() ?>Pages/pue" id="pue" class="nav_link"> <i class='bx bx-line-chart'></i> <span class="nav_name">PUE Harian</span> </a>
         </div>
