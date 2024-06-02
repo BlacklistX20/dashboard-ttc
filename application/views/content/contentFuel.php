a04ce36e68dbb23c3e81146037b053a12a4e9732
BlacklistX20 <a.syafrul.aswar20@gmail.com>
2024-06-03 05:34:48 +0800

    make tank value font larger

--- application/views/content/contentFuel.php
+++ application/views/content/contentFuel.php
@@ -1,9 +1,9 @@
 <div class="tank-container">
    <div class="tankDaily text-center cardCustom">
-      <h2>Tangki Harian</h2>
+      <h2 class="my-1 fw-bold">Tangki Harian</h2>
       <div class="row">
          <div class="col-6">
-            <p class="tank-label"><span id="tank1Value"></span> / <span id="tank1Max">1500</span></p>
+            <h3 class="tank-label my-3"><span id="tank1Value"></span> / <span id="tank1Max">1500</span></h3>
             <div class="tank-bar">
                <div id="tank1-level" class="tank-level d-flex justify-content-center align-items-center" style="background-color: #007bff;">
                   <h1 class="display-1 fw-bold" id="tank1-percent"></h1>
@@ -11,7 +11,7 @@
             </div>
          </div>
          <div class="col-6">
-            <p class="tank-label"><span id="tank2Value">1000</span> / <span>1500</span></p>
+            <h3 class="tank-label my-3"><span id="tank2Value"></span> / <span>1500</span></h3>
             <div class="tank-bar">
                <div id="tank2-level" class="tank-level d-flex justify-content-center align-items-center" style="background-color: #007bff;">
                   <h1 class="display-1 fw-bold" id="tank2-percent"></h1>
@@ -21,10 +21,10 @@
       </div>
    </div>
    <div class="tankmonthly text-center cardCustom">
-      <h2>Tangki Bulanan</h2>
+      <h2 class="my-1 fw-bold">Tangki Bulanan</h2>
       <div class="row">
          <div class="col-4">
-            <p class="tank-label"><span id="tank3Value">500</span> / <span>1000</span></p>
+            <h3 class="tank-label my-3"><span id="tank3Value">500</span> / <span>1000</span></h3>
             <div class="tank-bar">
                <div id="tank3-level" class="tank-level d-flex justify-content-center align-items-center" style="background-color: #007bff;">
                   <h1 class="display-1 fw-bold" id="tank3-percent"></h1>
@@ -32,7 +32,7 @@
             </div>
          </div>
          <div class="col-4">
-            <p class="tank-label"><span id="tank4Value">750</span> / <span>1000</span></p>
+            <h3 class="tank-label my-3"><span id="tank4Value">750</span> / <span>1000</span></h3>
             <div class="tank-bar">
                <div id="tank4-level" class="tank-level d-flex justify-content-center align-items-center" style="background-color: #007bff;">
                   <h1 class="display-1 fw-bold" id="tank4-percent"></h1>
@@ -40,7 +40,7 @@
             </div>
          </div>
          <div class="col-4">
-            <p class="tank-label"><span id="tank5Value">100</span> / <span>1000</span></p>
+            <h3 class="tank-label my-3"><span id="tank5Value">100</span> / <span>1000</span></h3>
             <div class="tank-bar">
                <div id="tank5-level" class="tank-level d-flex justify-content-center align-items-center" style="background-color: #007bff;">
                   <h1 class="display-1 fw-bold" id="tank5-percent"></h1>
